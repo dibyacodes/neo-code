cffc51237d193117033ca776c122ef5d3a6d2a06
dibyacodes <dibyacodes@gmail.com>
2025-09-22 07:26:07 +0530

    feat : Remapped the % to akey for making files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,16 @@ vim.keymap.set("n","<A-2>", vim.cmd.bnext)
 vim.keymap.set("n","<C-k>", ':m .-2<CR>',{noremap = true, silent = true})
 vim.keymap.set("n","<C-j>", ':m .+1<CR>',{noremap = true, silent = true})
 
+
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "netrw",
+  callback = function()
+    vim.keymap.set("n", "a", function()
+      local input = vim.fn.input("Please give filename: ")
+      if input ~= "" then
+        vim.cmd("edit " .. vim.fn.fnameescape(input))
+      end
+    end, { buffer = true })
+  end,
+})
